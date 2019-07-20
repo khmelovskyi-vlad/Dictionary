2c90461cc57ddfc874404387ddede296eb14a297
Bohdan <bohdan.hukivskyi@outlook.com>
2019-07-20 18:49:26 +0300

    Coordinates

---
+++ Dictionary/Dictionary/Coordinates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Coordinates
+    {
+        public Coordinates(int latitude, int longitude)
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        public override bool Equals(object obj)
+        {
+            return obj is Coordinates otherCoordinates && otherCoordinates.Latitude == this.Latitude && otherCoordinates.Longitude == this.Longitude;
+        }
+        public override int GetHashCode()
+        {
+            return Latitude.GetHashCode() ^ Longitude.GetHashCode();
+        }
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<Coordinates, double> ratingMap = new Dictionary<Coordinates, double>();
+            while (true)
+            {
+                Console.WriteLine("Enter you coordinate");
+
+                Console.WriteLine("Enter latitude");
+                var latitude = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter longitude");
+                var longitude = Convert.ToInt32(Console.ReadLine());
+
+                Coordinates coordinates = new Coordinates(latitude, longitude);
+                
+                if (ratingMap.TryGetValue(coordinates, out double numRating))
+                {
+                    Console.WriteLine($"Latitude = {latitude}, longitude = {longitude}, raiting this coordinat is {numRating}");
+                }
+                else
+                {
+                    Console.WriteLine("Don`t have raiting for this coordinate");
+                    Console.WriteLine("Enter rating to you coordinate");
+                    var raiting = Convert.ToDouble(Console.ReadLine());
+                    ratingMap.Add(coordinates, raiting);
+                    Console.WriteLine($"Latitude = {latitude}, longitude = {longitude}, raiting this coordinat is {raiting}");
+                }
+            }
+        }
+    }
+}
